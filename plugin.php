34fd0e07ecc0e03328317c0e0e7a249b5dc8103a
Awilum <sergey.romanenko@flextype.org>
2021-09-05 22:03:51 +0300

    feat(plugin): updates for plugin code base

--- plugin.php
+++ plugin.php
@@ -14,18 +14,18 @@ namespace Flextype\Plugin\Select2;
 /**
  * Add Blueprint block `Select2`
  */
-flextype('registry')->set('plugins.blueprints.settings.blocks.Select2', 
-                            flextype('registry')->get('plugins.select2.settings.blocks.Select2'));                
+registry()->set('plugins.blueprints.settings.blocks.Select2', 
+                            registry()->get('plugins.select2.settings.blocks.Select2'));                
 
 /**
  * Set Select2 locale
  */                         
-if (flextype('registry')->get('flextype.settings.locale') == 'en_US') {
+if (registry()->get('flextype.settings.locale') == 'en_US') {
     $locale = 'en';
-    flextype('registry')->set('plugins.select2.settings.blocks.Select2.properties.options.lang', $locale);
+    registry()->set('plugins.select2.settings.blocks.Select2.properties.options.lang', $locale);
 } else {
-    $locale = strings(flextype('registry')->get('flextype.settings.locale'))->lower()->substr(0, 2)->toString();
-    flextype('registry')->set('plugins.select2.settings.blocks.Select2.properties.options.lang', $locale);
+    $locale = strings(registry()->get('flextype.settings.locale'))->lower()->substr(0, 2)->toString();
+    registry()->set('plugins.select2.settings.blocks.Select2.properties.options.lang', $locale);
 }
 
 /**
@@ -38,12 +38,12 @@ if ($locale !== 'en') {
     $select2JS[] = 'project/plugins/select2/blocks/Select2/dist/lang/select2/langs/' . $locale . '.min.js';          
 } 
 
-if (flextype('registry')->get('plugins.select2.settings.assetsLoadOnAdmin')) {
-    flextype('registry')->set('assets.admin.js.select2', $select2JS);
-    flextype('registry')->set('assets.admin.css.select2', $select2CSS);
+if (registry()->get('plugins.select2.settings.assetsLoadOnAdmin')) {
+    registry()->set('assets.admin.js.select2', $select2JS);
+    registry()->set('assets.admin.css.select2', $select2CSS);
 }
 
-if (flextype('registry')->get('plugins.select2.settings.assetsLoadOnSite')) {
-    flextype('registry')->set('assets.site.js.select2', $select2JS);
-    flextype('registry')->set('assets.site.css.select2', $select2CSS);
+if (registry()->get('plugins.select2.settings.assetsLoadOnSite')) {
+    registry()->set('assets.site.js.select2', $select2JS);
+    registry()->set('assets.site.css.select2', $select2CSS);
 }
